eba4b57812a5eb58b39b6c04da056bf42f60cb68
amangalick <ahnika.mangalick@gmail.com>
2021-02-24 13:40:54 -0800

    Initial Commit

---
+++ Shared/Github_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_AppApp.swift
+//  Shared
+//
+//  Created by Ahnika Mangalick on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Github_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
